5f7d0c69d262da2f0589350f34601b9935283bc2
User-Dave <130862425+User-Dave@users.noreply.github.com>
2023-05-31 18:17:30 +0300

    Update SkipListExperimentUtils.java

--- SkipListExperimentUtils.java
+++ SkipListExperimentUtils.java
@@ -55,10 +55,57 @@ public class SkipListExperimentUtils {
     }
 
     public static double measureDeletions(AbstractSkipList skipList, int size) {
-        throw new UnsupportedOperationException("Replace this by your implementation");
+        size++;
+    	double sum = 0;
+        int[] arr = doubleArray(scrambleArray(size));
+        System.out.print("Array successfully generated:["); //TEST
+        for (int i : arr) System.out.print(i+",");
+        System.out.println("]");
+        for (int i = 0 ; i < size ; i++)
+        {
+        	System.out.println("i = "+i+", trying to find...");//TEST
+        	System.out.println("skipList.find(arr[i]) = "+skipList.find(arr[i]).key());//TEST
+        	double time = System.nanoTime();
+        	skipList.delete(skipList.find(arr[i]));
+        	System.out.println("Deleted successfully");
+        	time -= System.nanoTime();
+        	sum += time;
+        }
+        return sum/(size);
+    }
+
+    //HELP FUNCTION
+    private static int[] scrambleArray(int size)
+    {
+    	int[] arr = new int[size];
+    	for (int i = 0 ; i < size; i++)
+    		arr[i] = i;
+    	for (int i = 0 ; i < size; i++)
+    	{
+    		int temp = arr[i];
+    		int index = (int)(Math.random()*size);
+    		arr[i] = arr[index];
+    		arr[index] = temp;
+    	}
+    	return arr;
+    }
+    
+    private static int[] doubleArray(int[] arr)
+    {
+    	int[] ans = new int[arr.length];
+    	for (int i = 0 ; i < arr.length; i++)
+    		ans[i] = 2*arr[i];
+    	return ans;
     }
 
     public static void main(String[] args) {
+    	double[] probabilities = new double[] {0.33,0.5,0.75,0.9};
+    	int size = 4;
+    	for (double p : probabilities) {
+    		AbstractSkipList isl = measureInsertions(p, size).first();
+    		System.out.println(isl.toString());
+    		System.out.println(measureDeletions(isl, size));
+    	}
         System.exit(1); // Remove this line.
     }
 }
