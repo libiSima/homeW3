2f77a9bbaaaf3d1d3481586b275b6a0ece2eb475
libiSima <130863440+libiSima@users.noreply.github.com>
2023-06-11 17:06:59 +0300

    Update ChainedHashTable

--- ChainedHashTable.java
+++ ChainedHashTable.java
@@ -9,7 +9,8 @@ public class ChainedHashTable<K, V> implements HashTable<K, V> {
     final private double maxLoadFactor;
     private int capacity;
     private HashFunctor<K> hashFunc;
-
+    private List<List<Pair<K, V>>> hashTable;
+    private int n = 0;
 
     /*
      * You should add additional private members as needed.
@@ -20,6 +21,7 @@ public class ChainedHashTable<K, V> implements HashTable<K, V> {
     }
 
     public ChainedHashTable(HashFactory<K> hashFactory, int k, double maxLoadFactor) {
+        hashTable = new ArrayList<List<Pair<K, V>>>(k);
         this.hashFactory = hashFactory;
         this.maxLoadFactor = maxLoadFactor;
         this.capacity = 1 << k;
@@ -27,20 +29,58 @@ public class ChainedHashTable<K, V> implements HashTable<K, V> {
     }
 
     public V search(K key) {
-        throw new UnsupportedOperationException("Replace this by your implementation");
+        int index = hashFunc.hash(key);
+        List<Pair<K, V>> listf = hashTable.get(index);
+        for (Pair<K, V> pair : listf) {
+            if (pair.first() == key) {
+                return pair.second();
+            }
+        }
+        return null;
     }
 
     public void insert(K key, V value) {
-        throw new UnsupportedOperationException("Replace this by your implementation");
+        Pair<V, K> newp = new Pair(key, value);
+        int index = hashFunc.hash(key);
+        List listf = hashTable.get(index);
+        listf.add(newp);
+        n = n + 1;
+        if (n * maxLoadFactor > capacity) {
+            this.rehashing();
+        }
+
+    }
+
+    private void rehashing() {
+        this.hashFunc = hashFactory.pickHash(hashTable.size() * 2);
+        List<List<Pair<K, V>>> oldList = this.hashTable;
+        this.hashTable = new ArrayList<List<Pair<K, V>>>(hashTable.size() * 2);
+        this.capacity = this.capacity * 2;
+        for (List<Pair<K, V>> list : oldList) {
+            for (Pair<K, V> pair : list) {
+                this.insert(pair.first(), pair.second());
+            }
+        }
     }
 
     public boolean delete(K key) {
-        throw new UnsupportedOperationException("Replace this by your implementation");
+        int index = hashFunc.hash(key);
+        List<Pair<K, V>> listf = hashTable.get(index);
+        for (Pair<K, V> pair : listf) {
+            if (pair.first() == key) {
+                listf.remove(pair);
+                n = n - 1;
+                return true;
+            }
+        }
+        return false;
     }
 
     public HashFunctor<K> getHashFunc() {
         return hashFunc;
     }
 
-    public int capacity() { return capacity; }
+    public int capacity() {
+        return capacity;
+    }
 }
