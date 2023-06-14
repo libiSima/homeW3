c0d89cbabb23b366aa3f5c13578146bc3d9143d2
User-Dave <130862425+User-Dave@users.noreply.github.com>
2023-06-14 14:11:45 +0300

    Update ProbingHashTable.java

--- ProbingHashTable.java
+++ ProbingHashTable.java
@@ -14,6 +14,9 @@ public class ProbingHashTable<K, V> implements HashTable<K, V> {
     /*
      * You should add additional private members as needed.
      */
+    private int elements;
+    private ArrayList<Pair<K,V>> vals; //USE ARRAYLIST
+    private int kcurr;
 
     public ProbingHashTable(HashFactory<K> hashFactory) {
         this(hashFactory, DEFAULT_INIT_CAPACITY, DEFAULT_MAX_LOAD_FACTOR);
@@ -24,18 +27,50 @@ public class ProbingHashTable<K, V> implements HashTable<K, V> {
         this.maxLoadFactor = maxLoadFactor;
         this.capacity = 1 << k;
         this.hashFunc = hashFactory.pickHash(k);
+        // Added
+        this.elements = 0;
+        vals = new ArrayList<Pair<K,V>>(capacity);
+        for (int i = 0; i < capacity; i++)
+        	vals.add(null);
+        kcurr = k;
+        //ASK ABOUT INITIALIZING IT
     }
 
     public V search(K key) {
-        throw new UnsupportedOperationException("Replace this by your implementation");
+    	int h = hashFunc.hash(key);
+    	for (int i = 0 ; i < capacity; i++) {
+    		if (vals.get(h)!=null && vals.get(h).first().equals(key))
+    			return vals.get(h).second();
+    		h = (int)HashingUtils.fastModularPower(((long)h+1), ((long)1), ((long)capacity));
+    	}
+    	return null;
     }
 
     public void insert(K key, V value) {
-        throw new UnsupportedOperationException("Replace this by your implementation");
+    	int h = hashFunc.hash(key);
+    	for (int i = 0 ; i < capacity; i++) {
+    		if (vals.get(h) == null) {
+    			vals.set(h, new Pair<K,V>(key,value));
+    			elements += 1;
+    			if ((this.elements*1.0)/this.capacity > this.maxLoadFactor)
+    				rehash();
+    			return;
+    		}
+    		h = (int)HashingUtils.fastModularPower(((long)h+1), ((long)1), ((long)capacity));
+    	}
     }
 
     public boolean delete(K key) {
-        throw new UnsupportedOperationException("Replace this by your implementation");
+    	int h = hashFunc.hash(key);
+    	for (int i = 0 ; i < capacity; i++) {
+    		if (vals.get(h)!=null && vals.get(h).first().equals(key)) {
+    			vals.set(h, null);
+    			elements -= 1;
+    			return true;
+    		}
+    		h = (int)HashingUtils.fastModularPower(((long)h+1), ((long)1), ((long)capacity));
+    	}
+    	return false;
     }
 
     public HashFunctor<K> getHashFunc() {
@@ -43,4 +78,22 @@ public class ProbingHashTable<K, V> implements HashTable<K, V> {
     }
 
     public int capacity() { return capacity; }
+    
+    private void rehash()
+    {
+    	System.out.println("Rehashing with "+this.elements+" elements. (2^"+kcurr+" * maxMod = "+((1<<kcurr) * maxLoadFactor)+")");
+    	kcurr++;
+        this.capacity = 1<<kcurr;
+        this.hashFunc = hashFactory.pickHash(kcurr);
+        // Added
+        ArrayList<Pair<K,V>> old = this.vals;
+        this.vals = new ArrayList<Pair<K,V>>(capacity);
+        for (int i = 0; i < capacity; i++)
+        	vals.add(null);
+        for (int i = 0; i < old.size(); i++)
+        {
+        	if (old.get(i) != null) 
+        		this.insert(old.get(i).first(), old.get(i).second());
+        }
+    }
 }
