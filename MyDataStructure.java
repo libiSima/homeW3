299c81b06a40cfc0b92006c9b21ade165b6de4f5
User-Dave <130862425+User-Dave@users.noreply.github.com>
2023-06-18 16:26:55 +0300

    Update MyDataStructure.java

--- MyDataStructure.java
+++ MyDataStructure.java
@@ -1,5 +1,4 @@
 import java.util.List;
-
 import java.util.ArrayList;
 import java.util.LinkedList;
 
@@ -9,30 +8,32 @@ public class MyDataStructure {
      * Remember that all the data-structures you use must be YOUR implementations,
      * except for the List and its implementation for the operation Range(low, high).
      */
+	HashTable<Integer, AbstractSkipList.Node> hash;
+    IndexableSkipList skipList;
 
     /***
      * This function is the Init function described in Part 4.
      *
      * @param N The maximal number of items expected in the DS.
      */
-    HashTable<Integer, AbstractSkipList.Node> hash;
-    IndexableSkipList skipList;
-
     public MyDataStructure(int N) {
-        hash = new ChainedHashTable(new ModularHash());
+    	hash = new ChainedHashTable(new ModularHash());
         skipList = new IndexableSkipList(0.5);
     }
-        
+
     /*
      * In the following functions,
      * you should REMOVE the place-holder return statements.
      */
     public boolean insert(int value) {
-        return false;
+        if (this.contains(value)) return false;
+        this.skipList.insert(value);
+        this.hash.insert(value, skipList.search(value));
+        return true;
     }
 
     public boolean delete(int value) {
-        if(this.contains(value)){
+    	if(this.contains(value)){
             AbstractSkipList.Node tod = hash.search(value);
             skipList.delete(tod);
             return true;
@@ -41,27 +42,26 @@ public class MyDataStructure {
     }
 
     public boolean contains(int value) {
-        return false;
-
+        return (hash.search(value) != null);
     }
 
     public int rank(int value) {
-        return skipList.rank(value);
+    	return skipList.rank(value);
     }
 
     public int select(int index) {
-        return Integer.MIN_VALUE;
+        return skipList.select(index);
     }
 
     public List<Integer> range(int low, int high) {
-        if(! this.contains(low)){
+    	if(! this.contains(low)){
             return null;
         }
         AbstractSkipList.Node tod = hash.search(low);
         LinkedList<Integer> ans = new LinkedList();
         while(tod.key() <= high){
             ans.addLast(tod.key());
-            tod.getNext(0);
+            tod = tod.getNext(0);
 
         }
         return ans;
